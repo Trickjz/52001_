0c5f28383d4202b3fb8ea3c11379f6cfe6fa8c23
Trickjz <jeongyeonbias@gmail.com>
2025-05-17 16:27:49 -0300

    index.js

--- antlr-calculator-project/index.js
+++ antlr-calculator-project/index.js
@@ -1,62 +1,21 @@
-import CalculatorLexer from "./generated/CalculatorLexer.js";
-import CalculatorParser from "./generated/CalculatorParser.js";
-import { CustomCalculatorListener } from "./CustomCalculatorListener.js";
-import { CustomCalculatorVisitor } from "./CustomCalculatorVisitor.js";
-import antlr4, { CharStreams, CommonTokenStream, ParseTreeWalker } from "antlr4";
-import readline from 'readline';
-import fs from 'fs';
+const antlr4 = require('antlr4');
+const RegExpLexer = require('./generated/RegExpLexer').RegExpLexer;
+const RegExpParser = require('./generated/RegExpParser').RegExpParser;
+const CustomRegExpVisitor = require('./CustomRegExpVisitor');
 
-async function main() {
-    let input;
+const fs = require('fs');
 
-    // Intento leer la entrada desde el archivo input - en forma sincrona.
-    try {
-        input = fs.readFileSync('input.txt', 'utf8');
-    } catch (err) {
-        // Si no es posible leer el archivo, solicitar la entrada del usuario por teclado
-        input = await leerCadena(); // Simula lectura síncrona
-        console.log(input);
-    }
+// Recibe archivo de entrada como argumento
+const input = fs.readFileSync(process.argv[2]).toString();
 
-    // Proceso la entrada con el analizador e imprimo el arbol de analisis en formato texto
-    let inputStream = CharStreams.fromString(input);
-    let lexer = new CalculatorLexer(inputStream);
-    let tokenStream = new CommonTokenStream(lexer);
-    let parser = new CalculatorParser(tokenStream);
-    let tree = parser.prog();
-    
-    // Verifico si se produjeron errores
-    if (parser.syntaxErrorsCount > 0) {
-        console.error("\nSe encontraron errores de sintaxis en la entrada.");
-    } 
-    else {
-        console.log("\nEntrada válida.");
-        const cadena_tree = tree.toStringTree(parser.ruleNames);
-        console.log(`Árbol de derivación: ${cadena_tree}`);
+const chars = new antlr4.InputStream(input);
+const lexer = new RegExpLexer(chars);
+const tokens = new antlr4.CommonTokenStream(lexer);
+const parser = new RegExpParser(tokens);
+parser.buildParseTrees = true;
 
-        // Utilizo un listener y un walker para recorrer el arbol e indicar cada vez que reconoce una sentencia (stat)
-        //const listener = new CustomCalculatorListener();
-        // ParseTreeWalker.DEFAULT.walk(listener, tree);
+const tree = parser.prog(); // punto de inicio de la gramática
+const visitor = new CustomRegExpVisitor();
 
-        // Utilizo un visitor para visitar los nodos que me interesan de mi arbol
-        const visitor = new CustomCalculatorVisitor();
-        visitor.visit(tree);   
-    }
-}
-
-function leerCadena() {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    return new Promise(resolve => {
-        rl.question("Ingrese una cadena: ", (answer) => {
-            rl.close();
-            resolve(answer);
-        });
-    });
-}
-
-// Ejecuta la función principal
-main();
+const result = visitor.visit(tree);
+console.log("Resultado:", result);
