cb13e80387dcda88de53b630ee2e3217f9de6899
Trickjz <jeongyeonbias@gmail.com>
2025-05-18 00:33:40 -0300

    CustomRegExpVisitor.js

--- antlr-calculator-project/CustomRegExpVisitor.js
+++ antlr-calculator-project/CustomRegExpVisitor.js
@@ -2,11 +2,57 @@ const RegExpVisitor = require('./generated/RegExpVisitor').RegExpVisitor;
 
 class CustomRegExpVisitor extends RegExpVisitor {
     visitProg(ctx) {
-        console.log("Expresión válida.");
-        return null;
+        return this.visit(ctx.expr());
     }
 
-    // Podrías expandir más funciones como visitUnion, visitGroup, etc. si quisieras.
+    visitUnion(ctx) {
+        return {
+            tipo: "Union",
+            izquierda: this.visit(ctx.expr()),
+            derecha: this.visit(ctx.concat())
+        };
+    }
+
+    visitJustConcat(ctx) {
+        return this.visit(ctx.concat());
+    }
+
+    visitConcatExpr(ctx) {
+        return {
+            tipo: "Concat",
+            izquierda: this.visit(ctx.concat()),
+            derecha: this.visit(ctx.repeat())
+        };
+    }
+
+    visitJustRepeat(ctx) {
+        return this.visit(ctx.repeat());
+    }
+
+    visitKleene(ctx) {
+        return {
+            tipo: "Kleene",
+            valor: this.visit(ctx.atom())
+        };
+    }
+
+    visitNoRepeat(ctx) {
+        return this.visit(ctx.atom());
+    }
+
+    visitSingleChar(ctx) {
+        return {
+            tipo: "Char",
+            valor: ctx.getText()
+        };
+    }
+
+    visitGroup(ctx) {
+        return {
+            tipo: "Group",
+            contenido: this.visit(ctx.expr())
+        };
+    }
 }
 
-module.exports = CustomRegExpVisitor;
+module.exports = CustomRegExpVisitor; 
