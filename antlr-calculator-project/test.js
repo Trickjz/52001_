5269138290e8726840416b2d36f135f158f868e3
Trickjz <jeongyeonbias@gmail.com>
2025-05-18 20:42:42 -0300

    Update test.js

--- antlr-calculator-project/test.js
+++ antlr-calculator-project/test.js
@@ -1,19 +1,13 @@
-const fs = require('fs');
 const { execSync } = require('child_process');
+const inputs = ['input/ok1.txt', 'input/ok2.txt', 'input/err1.txt', 'input/err2.txt'];
 
-const inputs = [
-  'input/ok1.txt',
-  'input/ok2.txt',
-  'input/err1.txt',
-  'input/err2.txt'
-];
-
-for (const file of inputs) {
-  console.log(`\n===== Analizando ${file} =====`);
+inputs.forEach((file) => {
+  console.log(`\n--- Ejecutando ${file} ---`);
   try {
     const result = execSync(`node index.js ${file}`);
     console.log(result.toString());
   } catch (error) {
-    console.error(">> Error:", error.message);
+    console.error(`Error al procesar ${file}`);
+    console.error(error.stdout.toString());
   }
-}
+}}
