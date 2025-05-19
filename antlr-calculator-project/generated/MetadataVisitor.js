dc740cb49513214e003dff38c329627f93a5a7ce
Trickjz <jeongyeonbias@gmail.com>
2025-05-18 21:07:20 -0300

    Update MetadataVisitor.js

--- antlr-calculator-project/generated/MetadataVisitor.js
+++ antlr-calculator-project/generated/MetadataVisitor.js
@@ -1,2 +1,5 @@
-// Archivo generado automáticamente por ANTLR
+// Archivo generado automáticamente por ANTLR 4.13.2
 // Visitor base para Metadata.g4
+function MetadataVisitor() {
+  // Visitor para recorrer el árbol
+} 
